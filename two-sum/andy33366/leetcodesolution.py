21851fbe5e0f2983c641e96ff37a945842b5bff7
andy33366 <andy704389@gmail.com>
2025-04-05 18:19:12 -0500

    two-sum fixed

--- two-sum/andy33366/leetcodesolution.py
+++ two-sum/andy33366/leetcodesolution.py
@@ -5,59 +5,20 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
-
-        #solution is different for negative or positive target?
-        #Didn't finish it sorry :,(
-
-        #sort nums into pozz/neg and > or < target
-
-        #sorts target as pozz or neg, bool is set as true if pozz or 0
-        pozz = (target >= 0)
-
-        # if target is pozz
-        if pozz:
-
-            nums_poz_lt_target = {}
-            nums_neg = {}
-            nums_poz_gt_target = {}
-
-            #sorts nums into dictionary {<num>:<index>}
-            for i in nums:
-                if i < 0:
-                    #add num and index to nums_neg dictionary
-                    nums_neg[i] = nums.index(i)
-                elif i < target:
-                    nums_poz_lt_target[i] = nums.index(i)
-                else:
-                    nums_poz_gt_target[i] = nums.index(i)
-
-            #check if there are negative numbers
-            if not nums_neg:
-                for (i = 0, i < len(nums_poz_lt_target), i += 1):
-                    for (j = 0, i < len())
+        answer = []
 
 
+        for i in range(len(nums)):
 
+            #if complement is in the list, skips the rest
+            compliment = target - i
 
-            #nums are both pozz < target
+            if compliment in nums:
+                answer = [i, compliment]
+            #else, continue with the brute force
+            else:
+                for j in range(i+1, len(nums)):
+                    if nums[i] + nums[j] == target:
+                        answer = [i, j]
 
-            #or
-
-            #one num is pozz > target, the other num is neg
-
-        # if target is neg
-        else:
-            #nums are both neg > target
-
-            #or
-
-            #one num is pozz > target, the other is neg < target
-
-        #once sorted -->> bruteforce??
-
-        #positive target
-
-target = 8
-nums = [6, 5, 2, 9]
-test = Solution()
-test.twoSum(nums, target)
+        return answer
