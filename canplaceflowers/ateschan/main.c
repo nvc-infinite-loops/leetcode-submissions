2b2c151c8493f739f6c6a8ab579eb66335fd8bf5
Addison Teschan <89411709+ateschan@users.noreply.github.com>
2025-04-11 04:14:25 +0000

    Create main.c

---
+++ canplaceflowers/ateschan/main.c
@@ -0,0 +1,24 @@
+func canPlaceFlowers(flowerbed []int, n int) bool {
+	bedlength := len(flowerbed)
+	if bedlength == 1 {
+		if (n == 1 && flowerbed[0] == 0) || n == 0 {
+			return true
+		}
+		return false
+	}
+    
+	for i := 0; i < bedlength; i++ {
+        leftcheck := i == 0 || flowerbed[i - 1] == 0;
+        rightcheck := i == bedlength - 1 || flowerbed[i + 1] == 0;
+
+        if leftcheck && rightcheck && flowerbed[i] == 0{
+            flowerbed[i] = 1;
+            n -= 1;
+		}
+	}
+
+	if n > 0 {
+		return false
+	}
+	return true
+}
